78100b53f33361194100dec922e34ea5a35caae1
hyh05 <24520652@gm.uit.edu.vn>
2025-12-08 23:44:34 +0700

    operate

---
+++ mim/src/main/java/mim/local/mim/MimApplication.java
@@ -0,0 +1,13 @@
+package mim.local.mim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MimApplication.class, args);
+	}
+
+}
---
+++ mim/src/test/java/mim/local/mim/MimApplicationTests.java
@@ -0,0 +1,13 @@
+package mim.local.mim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
