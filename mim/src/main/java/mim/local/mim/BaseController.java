c1614d612b0a787ca86ba2260e6cec1506da9578
hyh05 <24520652@gm.uit.edu.vn>
2025-12-09 00:03:06 +0700

    Test database

--- mim/src/main/java/mim/local/mim/BaseController.java
+++ mim/src/main/java/mim/local/mim/BaseController.java
@@ -1,5 +1,6 @@
 package mim.local.mim; 
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/api")
 public class BaseController {
 
+    private JdbcTemplate jdbcTemplate;
+
     @GetMapping("test")
     public String test() {
-        return "Huy dep trai";
+        String sql = "CREATE TABLE IF NOT EXISTS test_table(\n" +
+             "    id INT PRIMARY KEY AUTO_INCREMENT,\n" +
+             "    name VARCHAR(100)\n" +
+             ")";
+jdbcTemplate.execute(sql);
+
+        return "Created test_table successfully.";
     }
 
 }
