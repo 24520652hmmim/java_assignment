e47e829cc98d3e74a0abecbd21e6f0e4a848f63e
hyh05 <24520652@gm.uit.edu.vn>
2025-12-12 00:38:52 +0700

    Login Configuration

--- mim/src/main/java/mim/local/mim/modules/users/dtos/LoginResponse.java
+++ mim/src/main/java/mim/local/mim/modules/users/dtos/LoginResponse.java
@@ -1,5 +1,24 @@
 package mim.local.mim.modules.users.dtos;
 
 public class LoginResponse {
+    private final String token;
+    private final UserDTOS user;
+
+    public LoginResponse(String token, UserDTOS user) {
+        this.token = token;
+        this.user = user;
+    }
+    public String getToken() {
+        return token;
+    }   
+    public void setToken(String token) {
+        this.token = token;
+    }
+    public UserDTOS getUser() {
+        return user;
+    }
+    public void setUser(UserDTOS user) {
+        this.user = user;
+    }
     
 }
